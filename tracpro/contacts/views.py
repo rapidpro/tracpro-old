203b6168c076e84222f40cd0a04107521afba481
Nic Pottier <nicpottier@gmail.com>
2014-11-14 17:13:41 -0500

    fix group names, DCO no longer needed

--- tracpro/contacts/views.py
+++ tracpro/contacts/views.py
@@ -7,7 +7,7 @@ class ContactListView(OrgPermsMixin, SmartTemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(ContactListView, self).get_context_data(**kwargs)
-        context['group'] = "%s DCO" % self.request.user.get_region()
+        context['group'] = "%s" % self.request.user.get_region()
         context['contacts'] = self.request.org.get_api().get_contacts(group=context['group'])
         return context
 
