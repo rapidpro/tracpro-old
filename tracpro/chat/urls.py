db8e7bb0801264184cf0f063c6d7fc46abfac9df
Rowan Seymour <rowanseymour@gmail.com>
2014-12-05 16:43:04 -0500

    Removed chat app

--- tracpro/chat/urls.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-
-from django.conf.urls import patterns, url
-from .views import ChatListView
-
-urlpatterns = patterns('',
-    url(r'^chat/', ChatListView.as_view(), name='chats.chat_list')
-)
